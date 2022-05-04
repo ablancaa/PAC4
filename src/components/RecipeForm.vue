12eb71f5056f43ca8e52a10866aa590456f97d77
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-05-05 01:03:02 +0200

    Mejoras en código , solo setup()

--- src/components/RecipeForm.vue
+++ src/components/RecipeForm.vue
@@ -66,22 +66,7 @@ import { uuid } from 'vue-uuid';
 //import axios from "axios";
 export default defineComponent({
   name: "RecipeForm",
-   /*data() {
-            return {
-                id: uuid.v1(),
-                title: '',
-                imageUrl: '',
-                servings: '',
-                time: '',
-                difficulty: '',
-                ingredients: [],
-                directions: [],
-                featured: '',
-                submit: '',
-                mensajeError: '',
-                recipe:{},
-            }
-        },*/
+
     emits:['nuevaReceta','closeForm'],
     setup(props, context){
       let showModal = ref(false);
@@ -95,7 +80,18 @@ export default defineComponent({
         ingredients: [],
         directions: [],
         featured: ''
-      })
+      });
+
+      /*  Aquest mètode ha d'executar-se quan es faci el submit del formulari, és a
+          dir, l'usuari faci click al botó Add Recipe, i s'encarregarà de:
+            ○ Comprovar que els camps title, ingredients i directions no estan buits. Si són buits
+              haureu de mostrar el missatge d'error prèviament descrit i no emetre la recepta.
+            ○ Separar el contingut dels camps ingredients i directions pel caràcter . (punt) i
+              afegir-los a un array.
+            ○ Crear un objecte (recipe) amb la informació guardada al formulari i els arrays
+              d'ingredients i directions..
+            ○ Emetre un esdeveniment add-recipe amb l'objecte creat.
+            ○ Esborrar els camps del formulari.  */
         const createRecipe = () => {
           var error = document.getElementById("errores");
           var add = document.getElementById("add");
@@ -155,169 +151,31 @@ export default defineComponent({
           //Indico en el Div add que se añade la receta nueva
               add.innerHTML = 'Add recipe <br/>';
               add.innerHTML += '<br/>';
-          //Datos introducidos mostrados por consola
-              // console.log("//DATOS INTRODUCIDOS EN FORMULARIO")
-              // console.log("Id: "+recipe.value.id);
-              // console.log("Title: "+recipe.value.title);
-              // console.log("imageURL: "+recipe.value.imageUrl);
-              // console.log("Servings: "+recipe.value.servings);
-              // console.log("Time: "+recipe.value.time);
-              // console.log("Difficulty: "+recipe.value.difficulty);
-              // console.log("Igredients: "+recipe.value.ingredients);
-              // console.log("Directions: "+recipe.value.directions);
-              // console.log("Featured: "+recipe.value.featured);
-              
+
           //Reinicio los campos
-              // document.getElementById("id").value = ' ';
-              // document.getElementById("title").value = ' ';
-              // recipe.value.id = '';
-              // recipe.value.title = '';
-              // recipe.value.imageUrl = ''; 
-              // recipe.value.servings = '';
-              // recipe.value.time = '';
-              // recipe.value.difficulty= '';
-              // recipe.value.ingredients = '';
-              // recipe.value.directions = '';
-              // recipe.value.featured = '';
+              recipe = ref({
+                id: recipe.value.id='', 
+                title: recipe.value.title='', 
+                imageUrl: recipe.value.imageUrl='', 
+                servings: recipe.value.servings='',
+                time: recipe.value.time='',
+                difficulty: recipe.value.difficulty='',
+                ingredients: recipe.value.ingredients=[],
+                directions: recipe.value.directions=[],
+                featured: recipe.value.featured='',
+              });
           }//Fin if/else
     
         }//FIN createRecipe()
-
+      /* Aquest mètode s'ha d'executar quan es faci clic al botó que conté el svg amb
+         el símbol X. S'encarregarà de:
+          ○ Emetre un esdeveniment close-modal  */
         const closeForm = () => {
           context.emit('closeForm', showModal.value = false);
-          //Chivato para ver si emite
-          //console.log("Función closeForm(){}: "+ this.showModal);
         }//FIN closeForm()
 
       return { createRecipe, closeForm, recipe }
       },
-        created(){},
-        mounted(){},
-        methods: {
-            /*  Aquest mètode ha d'executar-se quan es faci el submit del formulari, és a
-                dir, l'usuari faci click al botó Add Recipe, i s'encarregarà de:
-                  ○ Comprovar que els camps title, ingredients i directions no estan buits. Si són buits
-                    haureu de mostrar el missatge d'error prèviament descrit i no emetre la recepta.
-                  ○ Separar el contingut dels camps ingredients i directions pel caràcter . (punt) i
-                    afegir-los a un array.
-                  ○ Crear un objecte (recipe) amb la informació guardada al formulari i els arrays
-                    d'ingredients i directions..
-                  ○ Emetre un esdeveniment add-recipe amb l'objecte creat.
-                  ○ Esborrar els camps del formulari.  */
-          //  async createRecipe() {
-                // var error = document.getElementById("errores");
-                // var add = document.getElementById("add");
-                
-                // //Valida que los campos no esten vacíos
-                // if(this.title == '' || this.ingredients == '' || this.directions == '') {
-
-                // //Chivatos de campos vacios
-                  // console.log("Titulo vacio!!");
-                  // console.log("Ingredientes Vacio!!");
-                  // console.log("Indicaciones Vacio!!");
-
-                // //Pinta en pantalla el error
-                  // error.innerHTML = 'The fields Title, Ingredients and Directions are required <br/>';
-                  // error.innerHTML += '<br/>';
-                  // add.innerHTML = '';
-
-                // } else {
-
-                // //Separo ingredientes e indicaciones si tienen un punto                        
-                  // let ingredienteUnaAuno = this.ingredients;
-                  // ingredienteUnaAuno = this.ingredients.split('.');
-                  // console.log("Ingredientes Separados: "+ingredienteUnaAuno);
-                
-                  // let directionsUnaAuno = this.directions;
-                  // //directionsUnaAuno = this.directions.search('.');
-                  // directionsUnaAuno = this.directions.split('.');
-                  // //directionsUnaAuno = this.directions.split('\n');
-                 
-                  // console.log("Directions Separados: "+directionsUnaAuno);
-                // /* let directionsUnaAuno = this.directions;
-                  // directionsUnaAuno = this.directions.replace(/\s/g, ' .');
-                  // console.log("Directions Separados: "+directionsUnaAuno);
-                // */
-                // //Indico si tiene el featured activado
-                  // if(this.featured==''){
-                    // this.featured= false;
-                  // } else {
-                    // this.featured= true;
-                  // }
-                  
-                // //Creo el objeto receta para pasar a App 
-                  //  let recipe = {
-                        // id: this.id, 
-                        // title: this.title, 
-                        // imageUrl: this.imageUrl, 
-                        // servings: this.servings,
-                        // time: this.time,
-                        // difficulty: this.difficulty,
-                        // ingredients: ingredienteUnaAuno,
-                        // directions: directionsUnaAuno,
-                        // featured: this.featured,
-                    // };
-                      // try {
-                        // let response = await axios.post("http://localhost:3000/recipe/", recipe);
-                        // console.log("Response: "+response); 
-                        // //this.recipeList = response.data.recipe;
-                      // } catch (error){
-                        // console.log(error);
-                        // console.log("No funciona el borrado");
-                      // }
-
-                // //Emite la receta nueva a App
-                  //  this.$emit('nuevaReceta', recipe);
-
-                // //Cierra la ventana modal
-                  //  //this.$emit('cerrarForm', this.showModal=false);
-  
-                // //Muestra datos de receta por consola
-                    // console.log("Emitida nueva receta: "+recipe);
-
-                // //Pongo el Div de error en blanco
-                    // error.innerHTML = '';
-
-                // //Indico en el Div add que se añade la receta nueva
-                    // add.innerHTML = 'Add recipe <br/>';
-                    // add.innerHTML += '<br/>';
-                    
-                // //Datos introducidos mostrados por consola
-                    // console.log("//DATOS INTRODUCIDOS EN FORMULARIO")
-                    // console.log("Id: "+recipe.id);
-                    // console.log("Title: "+recipe.title);
-                    // console.log("imageURL: "+recipe.imageUrl);
-                    // console.log("Servings: "+recipe.servings);
-                    // console.log("Time: "+recipe.time);
-                    // console.log("Difficulty: "+recipe.difficulty);
-                    // console.log("Igredients: "+recipe.ingredients);
-                    // console.log("Directions: "+recipe.directions);
-                    // console.log("Featured: "+recipe.featured);
-                    
-                // //Reinicio los campos
-                    // this.id = '';
-                    // this.title= '';
-                    // this.imageUrl = ''; 
-                    // this.servings = '';
-                    // this.time = '';
-                    // this.difficulty = '';
-                    // this.ingredients = '';
-                    // this.directions = '';
-                    // this.featured = '';
-                    
-                // }//Fin if/else
-                
-            // },//FIN createRecipe()
-
-            /* Aquest mètode s'ha d'executar quan es faci clic al botó que conté el svg amb
-               el símbol X. S'encarregarà de:
-                ○ Emetre un esdeveniment close-modal  */
-            /*closeForm() {
-                this.$emit('closeForm', this.showModal = false);
-              //Chivato para ver si emite
-              //console.log("Función closeForm(){}: "+ this.showModal);
-            }//FIN closeForm()*/
-        },
 });
 </script>
 
